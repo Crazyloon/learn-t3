45c615ccefbb5bc0ca66f4006d3d7bbfda416e34
Russell Dow <russdow1@hotmail.com>
2023-01-08 02:00:00 -0800

    removed more boilerplate examples

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,5 +1,4 @@
 import { createTRPCRouter } from "./trpc";
-import { exampleRouter } from "./routers/example";
 import { getPokemon } from "./routers/get-pokemon";
 
 /**
@@ -8,7 +7,6 @@ import { getPokemon } from "./routers/get-pokemon";
  * All routers added in /api/routers should be manually added here
  */
 export const appRouter = createTRPCRouter({
-  example: exampleRouter,
   getPokemon: getPokemon,
 });
 
